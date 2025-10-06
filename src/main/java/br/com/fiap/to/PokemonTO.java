4e4f0693057cfb31c0c1d812b6669aeb5bdbf433
PxS00 <lucas.rossonidd@gmail.com>
2025-10-06 19:50:37 -0300

    feat: adiciona métodos getters e setters à classe PokemonTO

--- src/main/java/br/com/fiap/to/PokemonTO.java
+++ src/main/java/br/com/fiap/to/PokemonTO.java
@@ -22,4 +22,51 @@ public class PokemonTO {
         this.dataDaCaptura = dataDaCaptura;
     }
 
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public LocalDate getDataDaCaptura() {
+        return dataDaCaptura;
+    }
+
+    public void setDataDaCaptura(LocalDate dataDaCaptura) {
+        this.dataDaCaptura = dataDaCaptura;
+    }
 }
