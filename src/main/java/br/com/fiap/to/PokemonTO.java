f42635239f8049984cfcae90eb749f56efebc759
PxS00 <lucas.rossonidd@gmail.com>
2025-10-06 19:49:23 -0300

    feat: cria a classe PokemonTO com atributos básicos

---
+++ src/main/java/br/com/fiap/to/PokemonTO.java
@@ -0,0 +1,12 @@
+package br.com.fiap.to;
+
+import java.time.LocalDate;
+
+public class PokemonTO {
+    private Long codigo;
+    private String nome;
+    private Double altura;
+    private Double peso;
+    private String categoria;
+    private LocalDate dataDaCaptura;
+}
